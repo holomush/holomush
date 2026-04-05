0d81a0c8af7a2d7c3f6ad664a196bea03c741387
seanb4t <4678+seanb4t@users.noreply.github.com>
2026-04-05 16:27:04 -0400

    fix: add license headers to scene plugin SQL migrations

--- plugins/core-scenes/migrations/000001_scenes.down.sql
+++ plugins/core-scenes/migrations/000001_scenes.down.sql
@@ -1,3 +1,6 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Copyright 2026 HoloMUSH Contributors
+
 DROP INDEX IF EXISTS idx_scene_logs_scene;
 DROP INDEX IF EXISTS idx_scene_participants_character;
 DROP INDEX IF EXISTS idx_scenes_location;
--- plugins/core-scenes/migrations/000001_scenes.up.sql
+++ plugins/core-scenes/migrations/000001_scenes.up.sql
@@ -1,3 +1,6 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Copyright 2026 HoloMUSH Contributors
+
 CREATE TABLE IF NOT EXISTS scenes (
     id               TEXT        PRIMARY KEY,
     title            TEXT        NOT NULL,
