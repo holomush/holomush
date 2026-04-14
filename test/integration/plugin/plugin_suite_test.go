14a8336c0e2581b82105b8674fef57faa43dd451
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2026-04-13 22:19:24 -0400

    refactor(test): share container in content store tests (1 → 0 new)

--- test/integration/plugin/plugin_suite_test.go
+++ test/integration/plugin/plugin_suite_test.go
@@ -10,9 +10,19 @@ import (
 
 	. "github.com/onsi/ginkgo/v2" //nolint:revive // ginkgo convention
 	. "github.com/onsi/gomega"    //nolint:revive // gomega convention
+
+	"github.com/holomush/holomush/test/testutil"
 )
 
+var suiteT *testing.T
+var sharedPG *testutil.PostgresEnv
+
 func TestBinaryPlugin(t *testing.T) {
+	suiteT = t
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Binary Plugin Integration Suite")
 }
+
+var _ = BeforeSuite(func() {
+	sharedPG = testutil.SharedPostgres(suiteT)
+})
