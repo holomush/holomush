f6195eafe366c2ee063311cbeafe5722de59e36e
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2026-05-16 11:17:33 -0400

    test(totp): new Ginkgo bootstrap + convert repo_integration_test (1hq.26 B1.3)

---
+++ internal/totp/totp_suite_test.go
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026 HoloMUSH Contributors
+
+//go:build integration
+
+package totp_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2" //nolint:revive // ginkgo convention
+	. "github.com/onsi/gomega"    //nolint:revive // gomega convention
+
+	"github.com/holomush/holomush/test/testutil"
+)
+
+var (
+	suiteT   *testing.T
+	sharedPG *testutil.PostgresEnv
+)
+
+func TestTOTPIntegration(t *testing.T) {
+	suiteT = t
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "TOTP Integration Suite")
+}
+
+var _ = BeforeSuite(func() {
+	sharedPG = testutil.SharedPostgres(suiteT)
+})
